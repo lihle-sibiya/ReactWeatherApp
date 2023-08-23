ccafec9d97fe2600c717e46cd6f821099ac26154
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-23 02:21:15 +0200

    fifth commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
-//import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/database';
 import WeatherApp from './WeatherApp';
 
 class App extends React.Component {
